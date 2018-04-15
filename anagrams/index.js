aefd5b905d6c43f5ce731ee21ed71e91a93ceacb
apolon1121 <a.pachulia@gmail.com>
2018-04-15 21:16:02 +0400

    formmated

--- anagrams/index.js
+++ anagrams/index.js
@@ -1,17 +1,17 @@
 function anagrams(stringA, stringB) {
-    const aCharMap=buildCharMap(stringA);
-    const bCharMap=buildCharMap(stringB);
-   }
-   
-   function buildCHarMap(str) {
-     const charMap = {};
-     
-     for (let char of str.replace(/[^\w]/g,"").toLowerCase()) {
-       charMap[char] =  charMap[char] + 1 || 1;
-     }
-     
-     return charMap;
-     
-   }
-   
-   console.log(anagrams("Apolon Pachulia","apolon"));
+	const aCharMap = buildCharMap(stringA);
+	const bCharMap = buildCharMap(stringB);
+}
+
+function buildCHarMap(str) {
+	const charMap = {};
+
+	for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+
+	return charMap;
+
+}
+
+console.log(anagrams("Apolon Pachulia", "apolon"));
