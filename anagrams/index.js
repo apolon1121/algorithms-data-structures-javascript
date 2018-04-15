bc1a82d4aa4b6f5e899b8876a90056e384cb6aa7
apolon1121 <a.pachulia@gmail.com>
2018-04-15 21:22:23 +0400

    anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -1,17 +1,26 @@
 function anagrams(stringA, stringB) {
 	const aCharMap = buildCharMap(stringA);
-	const bCharMap = buildCharMap(stringB);
-}
+    const bCharMap = buildCharMap(stringB);
+    
+    if(Object.keys(aCharMap).length !==  Object.keys(bCharMap).length) {
+        return false;
+    }
 
-function buildCHarMap(str) {
+    for(let char in aCharMap) {
+        if(aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
+ }
+
+function buildCharMap(str) {
 	const charMap = {};
 
 	for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
 		charMap[char] = charMap[char] + 1 || 1;
 	}
-
 	return charMap;
-
 }
-
-console.log(anagrams("Apolon Pachulia", "apolon"));
+console.log(anagrams("paChul ia", "pachuLia"));
