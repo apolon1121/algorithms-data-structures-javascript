4be43893a9a841e8eca42f9128f2d8b5b13ead94
apolon1121 <a.pachulia@gmail.com>
2018-04-15 21:55:58 +0400

    anagram second version

--- anagrams/2.js
+++ anagrams/2.js
@@ -1,6 +1,10 @@
 function anagrams(stringA, stringB) {
-	
+	return cleanString(stringA) === cleanString(stringB);
+ }
+
+ function cleanString(str) {
+     return str.replace(/[^/w]]/g,'').toLowerCase().split('').sort().join('');
  }
 
 
-console.log(anagrams("paChul ia", "pachuLia"));
+console.log(anagrams("apolon", "nolopa"));
